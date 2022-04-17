34c909156ff6b2ab60cb272fc8d76449a7750fdb
sadik126 <sadikhimel04@gmail.com>
2022-04-18 04:43:21 +0600

    m

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,10 +1,41 @@
+import { Button } from 'bootstrap';
 import React from 'react';
+import { Container, Form, FormControl, InputGroup, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 
 const Header = () => {
     return (
-        <div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand fw-bold" href="#"> <span className='text-info'> Wild </span>Studio</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li className="nav-item me-4">
+                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
 
-        </div>
+                        <li className="nav-item me-4">
+                            <a className="nav-link" href="#">Services</a>
+                        </li>
+                        <li className="nav-item me-4">
+                            <a className="nav-link" href="#">About</a>
+                        </li>
+                        <li className="nav-item me-4">
+                            <a className="nav-link" href="#">Login</a>
+                        </li>
+                        <li className="nav-item me-4">
+                            <a className="nav-link" href="#">Signup</a>
+                        </li>
+
+
+                    </ul>
+
+                </div>
+            </div>
+        </nav>
+
     );
 };
 
