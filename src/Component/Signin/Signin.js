36baa38bac40bcba60f11083ec5a4b543467fa7a
sadik126 <sadikhimel04@gmail.com>
2022-04-18 17:35:34 +0600

    signup

--- src/Component/Signin/Signin.js
+++ src/Component/Signin/Signin.js
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Signin = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const nevigate = useNavigate()
+
+
+    const handleemail = (e) => {
+        setEmail(e.target.value)
+
+    }
+
+    const Handlepass = (e) => {
+        setPassword(e.target.value)
+    }
+
+    const Handleusesignin = (e) => {
+        e.preventDefault()
+        console.log(email, password)
+
+    }
+
+    const Navigateregister = () => {
+        nevigate('/signup')
+    }
     return (
-        <div>
-            <h1>Please log in</h1>
-            <Form>
+        <div className='container w-50 mx-auto mt-5'>
+            <h1 className='text-center'>Please Login  here</h1>
+            <Form onSubmit={Handleusesignin}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control onBlur={handleemail} type="email" placeholder="Enter email" />
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
@@ -16,7 +40,7 @@ const Signin = () => {
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control onBlur={Handlepass} type="password" placeholder="Password" />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Check me out" />
@@ -25,6 +49,7 @@ const Signin = () => {
                     Submit
                 </Button>
             </Form>
+            <p>New to wild studio?</p> <Link to="/signup" className='text-danger pe-auto text-decoration-none' onClick={Navigateregister}>Please register</Link>
         </div>
     );
 };
--- src/Component/Signup/Signup.js
+++ src/Component/Signup/Signup.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Signup = () => {
+    const nevigate = useNavigate()
+    const Navigateregister = () => {
+        nevigate('/signin')
+    }
     return (
-        <div>
-            <h1>this is signup</h1>
+        <div className='container w-50 mx-auto mt-5'>
+            <h1>Please register here</h1>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicName">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type="email" placeholder="Enter name" />
+
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+
+                <Button variant="success" type="submit">
+                    Register
+                </Button>
+            </Form>
+            <p>New to wild studio?</p> <Link to="/signin" className='text-danger pe-auto text-decoration-none' onClick={Navigateregister}>Go to login form</Link>
         </div>
     );
 };
