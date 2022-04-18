adcbfa9575d37cf39699f5b84171b925fd172d4d
sadik126 <sadikhimel04@gmail.com>
2022-04-18 06:42:36 +0600

    m

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -2,7 +2,35 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
+        <div className='container-fluid bg-dark text-light'>
+            <div className="row">
+                <div className="col-md-3">
+                    <h5>Follow me</h5>
+                    <a href="">Facebook</a><br />
+                    <a href="">Linkdin</a><br />
+                    <a href="">Instagram</a><br />
+                    <a href="">Github</a><br />
+
+                </div>
+                <div class="col-md-3">
+                    <h5>My address</h5>
+                    <p>Mirpur-14,Ibrahimpur,Kamal khan sarak,Dhaka-1206</p>
+                    <p>phone:+8801400156406</p>
+                    <p>Email:sadikhimel04@gmail.com</p>
+                </div>
+                <div class="col-md-3">
+                    <h5>Information</h5>
+                </div>
+                <div class="col-md-3">
+                    <h5>legal</h5>
+                </div>
+            </div>
+            <div class="row p-2 bg-dark">
+                <div class="col-md-12">
+                    <h6 class="text-center">© 2021 Copyright: Rayhan sadik</h6>
+                </div>
+            </div>
+
 
         </div>
     );
